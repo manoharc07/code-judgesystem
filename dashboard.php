9989c24ecb8304e9e3d0cb930eec340d99f696ea
manoharc07 <manoharc07@gmail.com>
2020-03-30 00:45:21 +0530

    finished all linking for norm user except to editor

--- dashboard.php
+++ dashboard.php
@@ -1,7 +1,7 @@
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="CSS/style.css" />
-  <link rel="stylesheet" href="CSS/problemstyle.css" />
+  <link rel="stylesheet" href="CSS/tablestyle.css" />
   <link
     href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap"
     rel="stylesheet"
@@ -97,8 +97,30 @@
   </nav>
 
   <main>
-
-
+    <div="container">
+    <table class="content-table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Score</th>
+          <th>Difficulty</th>
+          <th>Acceptance</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        include_once 'dbConnector.php';
+        $sql="SELECT prob_id,prob_name,score FROM problems";
+        $count=1;
+        $result=mysqli_query($conn,$sql);
+        while($row=mysqli_fetch_assoc($result)){
+          echo "<tr><td>$count</td><td><a href='editor.html'>".$row['prob_name']."</a></td><td>".$row['score']."</td><td></td><td></td></tr>";
+          $count=$count+1;
+        }
+        ?>
+      </tbody>
+    </table>
     </div>
   </main>
 </body>
