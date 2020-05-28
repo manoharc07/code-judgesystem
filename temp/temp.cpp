eda9cec2a90ce5056b3c53aabc91000c74c5c354
manoharc07 <manoharc07@gmail.com>
2020-05-28 22:00:42 +0530

    none

--- temp/temp.cpp
+++ temp/temp.cpp
@@ -1,1 +1,30 @@
-efss
+#include<bits/stdc++.h>
+using namespace std;
+string isBalanced(string s) {
+    stack<char> st;
+    st.push('@');
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+            st.push(s[i]);
+            continue;
+        }    
+        if((s[i]=='}'&&st.top()=='{')||(s[i]==')'&&st.top()=='(')||(s[i]==']'&&st.top()=='['))
+            st.pop();
+        else {
+            return "NO";
+        }    
+    }
+    if(st.top()=='@')
+        return "YES";
+    return "NO";
+}
+int main(){
+    int t;
+    string s;
+    cin>>t;
+    while(t--){
+        cin>>s;
+        cout<<isBalanced(s)<<endl;
+    }
+    return 0;
+}
